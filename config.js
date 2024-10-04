fdc82ad86a4f3ad3b2c32091e80c601cc00bd0af
Paolo Compagnini <paolo.compagnini@gmail.com>
2024-10-04 14:07:43 +0200

    update config

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
-    datadir: "./wkd",
-    ServerDefaultKey: "./misc/domain.eu.server.key",
-    ServerDefaultCert: "./misc/domain.eu.server.cert",
-    pgpprivkey: "./misc/domain.eu.pgp.cert",
-    pgppubkey: "./misc/domain.eu.pgp.key",
+    datadir: "./",
+    ServerDefaultKey: "/etc/letsencrypt/live/openpgpkey.positron-it.de/privkey.pem",
+    ServerDefaultCert: "/etc/letsencrypt/live/openpgpkey.positron-it.de/cert.pem",
+    pgpprivkey: "./submission.key",
+    pgppubkey: "./positron-it.de/hu/54f6ry7x1qqtpor16txw5gdmdbbh6a73",
     pgpkeypass: '',
     smtp: {
         sendmail: true,
-        mailaddress: "key-submission@domain.eu",
+        mailaddress: "key-submission@positron-it.de",
         port: 25,
         host: '',
         auth: {
@@ -16,6 +16,10 @@ const config = {
         }
     },
     domains: {
+        "positron-it.de": {
+            cert: "/etc/letsencrypt/live/openpgpkey.positron-it.de/privkey.pem",
+            key:  "/etc/letsencrypt/live/openpgpkey.positron-it.de/cert.pem"
+        },
         "example.eu": {
             cert: "./misc/domain.eu.server.cert",
             key:  "./misc/domain.eu.server.key"
